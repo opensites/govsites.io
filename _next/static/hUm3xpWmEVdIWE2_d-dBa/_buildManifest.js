9b15cf7c9e04bd9a4852baa944812abf27d01bbc
tenfromfive <tenfromfive@users.noreply.github.com>
2022-03-15 16:33:52 +0000

    deploy: 39c6c98a891b91e5245374b35be05f3ec018de9f

--- _next/static/hUm3xpWmEVdIWE2_d-dBa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-c833d707bfe650efb126.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c4e9cb50a78c36ee5c26.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
